ff270877e7b3ddd8bc65a0e62925992fb664bf85
Sophie Thomson <sophielbenoit@gmail.com>
2024-11-25 15:51:14 +0000

    Add sign up form and axios defaults

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-// import Button from 'react-bootstrap/Button';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import {Route,Switch} from 'react-router-dom';
+import './api/axiosDefaults';
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
     <div className={styles.App}>
-        {/* <Button variant="primary">Primary</Button> */}
         <NavBar />
+        <Container className={styles.Main}>
+          <Switch>
+              <Route exact path="/" render={() => <h1>Home Page</h1>} />
+              <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+              <Route exact path="/signup" render={() => <SignUpForm />} />
+              <Route render={() => <p>Page not found!</p>} />
+          </Switch>
+        </Container>
     </div>
   );
 }
---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://django-rest-unit-e67236d699b8.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
