d7097c0a91aa5662fca820e1c799a5d4c696ab4f
Sophie Thomson <sophielbenoit@gmail.com>
2024-11-27 13:14:42 +0000

    Add CurrentUserContext in contexts folder and update sxios interceptors to enable refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
-axios.defaults.baseURL = 'https://django-rest-unit-e67236d699b8.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'https://django-rest-unit-e67236d699b8.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
+
+// Interceptors
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
