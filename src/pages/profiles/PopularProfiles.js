775ecd69d8b5a1e2fe80eb688b04e9a1a1e3d1ab
Sophie Thomson <sophielbenoit@gmail.com>
2024-12-02 17:24:20 +0000

    Customise PopularPorfiles.js to display responsive versions of top profiles for mobile and desktop.

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,51 +1,63 @@
-import React, { useEffect, useState } from 'react'
-import appStyles from '../../App.module.css'
-import { Container } from 'react-bootstrap'
-import { axiosReq } from '../../api/axiosDefaults';
-import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import { axiosReq } from "../../api/axiosDefaults";
+import appStyles from "../../App.module.css";
 import Asset from "../../components/Asset";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
+const PopularProfiles = ({ mobile }) => {
+  const [profileData, setProfileData] = useState({
+    // we will use the pageProfile later!
+    pageProfile: { results: [] },
+    popularProfiles: { results: [] },
+  });
+  const { popularProfiles } = profileData;
+  const currentUser = useCurrentUser();
 
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/profiles/?ordering=-followers_count"
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          popularProfiles: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-const PopularProfiles = () => {
-    const [profileData, setProfileData] = useState({
-        pageProfile: { results: [] },
-        popularProfiles: { results: [] },
-    });
-    const { popularProfiles } = profileData;
-    const currentUser = useCurrentUser
+    handleMount();
+  }, [currentUser]);
 
-    useEffect (() => {
-        const handleMount = async () => {
-            try {
-                const {data} = await axiosReq.get (
-                    "/profiles/?ordering=-followers_count"
-                );
-                setProfileData(prevState => ({
-                    ...prevState,
-                    popularProfiles: data,
-                }));
-            } catch (err) {
-                console.log(err)
-            }
-        };
-        handleMount()
-    }, [currentUser]);
-
-    return (
-        <Container className={appStyles.Content}>
-          {popularProfiles.results.length ? (
-            <>
-              <p>Most followed profiles.</p>
-              {popularProfiles.results.map((profile) => (
+  return (
+    <Container
+      className={`${appStyles.Content} ${
+        mobile && "d-lg-none text-center mb-3"
+      }`}
+    >
+      {popularProfiles.results.length ? (
+        <>
+          <p>Most followed profiles.</p>
+          {mobile ? (
+            <div className="d-flex justify-content-around">
+              {popularProfiles.results.slice(0, 3).map((profile) => (
                 <p key={profile.id}>{profile.owner}</p>
               ))}
-            </>
+            </div>
           ) : (
-            <Asset spinner />
+            popularProfiles.results.map((profile) => (
+              <p key={profile.id}>{profile.owner}</p>
+            ))
           )}
-        </Container>
-    );
+        </>
+      ) : (
+        <Asset spinner />
+      )}
+    </Container>
+  );
 };
 
-export default PopularProfiles
+export default PopularProfiles;
